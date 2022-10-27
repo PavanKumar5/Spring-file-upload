6b9a5e13a62e57c9a5cb30610970c55b6f9bb8b9
Pavan Kumar P <pavan.r834@gmail.com>
2022-10-28 01:01:26 +0530

    [1] - Created REST endpoints to upload a file to local storage

---
+++ src/main/java/springboot/example/fileuploadanddownload/ApplicationToUploadAndDownloadTheFileApplication.java
@@ -0,0 +1,13 @@
+package springboot.example.fileuploadanddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationToUploadAndDownloadTheFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationToUploadAndDownloadTheFileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/springboot/example/fileuploadanddownload/resource/FileResource.java
@@ -0,0 +1,48 @@
+package springboot.example.fileuploadanddownload.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.Files.copy;
+import static java.nio.file.Paths.get;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
+
+
+@RestController
+@RequestMapping("/file")
+public class FileResource {
+    // define a location
+    public static final String DIRECTORY = System.getProperty("user.home") + "/Downloads";
+
+    // Define a method to upload files
+    @PostMapping("/upload")
+    public ResponseEntity<List<String>> uploadFiles(@RequestParam("files")List<MultipartFile> multipartFiles) throws IOException {
+        List<String> filenames = new ArrayList<>();
+        for(MultipartFile file : multipartFiles) {
+            String filename = StringUtils.cleanPath(file.getOriginalFilename());
+            Path fileStorage = get(DIRECTORY, filename).toAbsolutePath().normalize();
+            copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
+            filenames.add(filename);
+        }
+        return ResponseEntity.ok().body(filenames);
+    }
+
+}

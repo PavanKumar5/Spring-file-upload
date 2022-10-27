e8a88660806f2cb60d124037094e34509595c82f
Pavan Kumar P <pavan.r834@gmail.com>
2022-10-28 01:24:05 +0530

    [1] - Refactored the file name and adding error handling

--- src/main/java/springboot/example/fileuploadanddownload/controller/FileController.java
+++ src/main/java/springboot/example/fileuploadanddownload/controller/FileController.java
@@ -1,21 +1,13 @@
-package springboot.example.fileuploadanddownload.resource;
+package springboot.example.fileuploadanddownload.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +15,11 @@ import java.util.List;
 import static java.nio.file.Files.copy;
 import static java.nio.file.Paths.get;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
 
 
 @RestController
 @RequestMapping("/file")
-public class FileResource1 {
+public class FileController {
     // define a location
     public static final String DIRECTORY = System.getProperty("user.home") + "/Downloads";
 
@@ -36,13 +27,19 @@ public class FileResource1 {
     @PostMapping("/upload")
     public ResponseEntity<List<String>> uploadFiles(@RequestParam("files")List<MultipartFile> multipartFiles) throws IOException {
         List<String> filenames = new ArrayList<>();
-        for(MultipartFile multipartFile : multipartFiles) {
-            String filename = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-            Path fileStorage = get(DIRECTORY, filename).toAbsolutePath().normalize();
-            copy(multipartFile.getInputStream(), fileStorage, REPLACE_EXISTING);
-            filenames.add(filename);
+        try {
+            for (MultipartFile file : multipartFiles) {
+                if (file.getOriginalFilename() != null) {
+                    String filename = StringUtils.cleanPath(file.getOriginalFilename());
+                    Path fileStorage = get(DIRECTORY, filename).toAbsolutePath().normalize();
+                    copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
+                    filenames.add(filename);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            filenames.add(e.getMessage());
         }
         return ResponseEntity.ok().body(filenames);
     }
-
 }
